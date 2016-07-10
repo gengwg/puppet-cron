b682325259697bbf7b46b26cb38f519cd059d5df
Weigang Geng <gengwg@gmail.com>
2016-07-09 23:15:31 -0700

    add manage crond configs also

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,24 @@
 
 class cron (
   $package_ensure = 'installed'
+  $service_config_path      = $cron::params::service_config_path,
+  $service_config_template  = $cron::params::service_config_template,
+  $cron_allow               = [],
+  $cron_deny                = [],
+  $send_to_syslog = $cron::params::send_to_syslog,
+  $send_to_mail = $cron::params::send_to_mail,
+
 ) {
   class { '::cron::install': package_ensure => $package_ensure }
+
+  file { '/etc/sysconfig/crond':
+    ensure  => present,
+    path    => $service_config_path,
+    content => template($service_config_template),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
 }
 
